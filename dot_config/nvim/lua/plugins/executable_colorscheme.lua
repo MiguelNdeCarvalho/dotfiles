445ca466f2d93fd25d3034bd450e1723c49f0e2c
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2025-12-12 00:37:17 +0000

    Initial commit

---
+++ dot_config/nvim/lua/plugins/executable_colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add catppuccin
+  { "catppuccin/nvim" },
+
+  -- Configure LazyVim to load nord
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+}
